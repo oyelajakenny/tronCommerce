b2c8bf7dc589908affbebf2eb029c83211b9ff18
Oyelaja Hussein <premiumitng@gmail.com>
2024-12-19 17:07:12 +0100

    Added Mobile Menu Component

---
+++ components/Mobilemenu.tsx
@@ -0,0 +1,13 @@
+import { AlignLeft } from 'lucide-react'
+import React from 'react'
+
+const Mobilemenu = () => {
+  return (
+    <div>
+        <AlignLeft className='hover:text-darkColor hoverEffect md:hidden' />
+
+    </div>
+  )
+}
+
+export default Mobilemenu
