fdeb06190ac6db5c27dbf3f6dca2805f58786b46
Oyelaja Hussein <premiumitng@gmail.com>
2024-12-22 22:53:46 +0100

    Created product page

---
+++ app/(client)/product/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  Container  from '@/components/Container'
+
+const SingleProductPage = () => {
+  return (
+    <div>
+        <Container>SingleProductPage</Container>
+    </div>
+  )
+}
+
+export default SingleProductPage;
