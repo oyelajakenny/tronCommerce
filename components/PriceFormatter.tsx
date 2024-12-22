f79727397b90ad1f1ab4e81318e1392d4fe5d23a
Oyelaja Hussein <premiumitng@gmail.com>
2024-12-22 22:47:44 +0100

    Added a component to format products price

---
+++ components/PriceFormatter.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { cn } from "@/lib/utils";
+
+interface Props {
+    amount: number | undefined;
+    className?: string;
+}
+const PriceFormatter = ({amount, className}:Props) => {
+    const formattedPrice = new Number(amount).toLocaleString('en-US', {currency: 'USD', style: 'currency', minimumFractionDigits: 2})
+  return (
+    <span className={cn("text-sm text-darkColor", className)}>{formattedPrice}</span>
+  )
+}
+
+export default PriceFormatter
