c48bc7123f02ae33627f8741dd2a121e3c96a053
Oyelaja Hussein <premiumitng@gmail.com>
2024-12-23 22:53:49 +0100

    Modified

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -1,12 +1,100 @@
-import React from 'react'
-import  Container  from '@/components/Container'
+import React from "react";
+import Container from "@/components/Container";
+import { getProductBySlug } from "@/sanity/helpers/query";
+import { notFound } from "next/navigation";
+import ImageView from "@/components/ImageView";
+import PriceView from "@/components/PriceView";
+import AddToCartButton from "@/components/AddToCartButton";
+import {
+  BoxIcon,
+  FileQuestion,
+  Heart,
+  ListOrderedIcon,
+  Share,
+} from "lucide-react";
+import ProductCharacteristics from "@/components/ProductCharacteristics";
 
-const SingleProductPage = () => {
+const SingleProductPage = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params;
+  const product = await getProductBySlug(slug);
+  if (!product) {
+    return notFound();
+  }
   return (
-    <div>
-        <Container>SingleProductPage</Container>
-    </div>
-  )
-}
+    <Container className="py-10 flex flex-col md:flex-row gap-10 ">
+      {product?.images && <ImageView images={product?.images} />}
+      <div className="w-full md:w-1/2 flex flex-col gap-5 ">
+        <div>
+          <h2 className="text-3xl md:text-4xl font-bold mb-2">
+            {product?.name}
+          </h2>
+          <PriceView
+            price={product?.price}
+            discount={product?.discount}
+            className="text-lg font-bold"
+          />
+        </div>
+        {product?.stock && (
+          <p className="bg-green-100 w-24 text-center text-green-600 text-sm py-2.5 font-semibold rounded-lg  ">
+            In Stock
+          </p>
+        )}
+        <p className="text-sm text-gray-600 tracking-wide">
+          {product?.description}
+        </p>
+        <div className="flex items-center gap-2.5 lg:gap-5 w-full">
+          <AddToCartButton
+            product={product}
+            className="bg-darkColor/80 text-white hover:bg-darkColor hoverEffect"
+          />
+          <button className="border-2 border-darkColor/30 text-darkColor/60 px-2.5 py-1.5 rounded-md">
+            <Heart className="w-5 h-5" />
+          </button>
+        </div>
+        <ProductCharacteristics product={product} />
+        <div className="flex flex-wrap items-center justofy-between gap-2.5 border-b border-b-gray-200 py-5 -mt-2">
+          <div className="flex items-center gap-2 text-sm text-black hover:text-red-600 hoverEffect">
+            <BoxIcon />
+            <p>Compare color</p>
+          </div>
+          <div className="flex items-center gap-2 text-sm text-black hover:text-red-600 hoverEffect">
+            <FileQuestion />
+            <p>Ask a question</p>
+          </div>
+          <div className="flex items-center gap-2 text-sm text-black hover:text-red-600 hoverEffect">
+            <ListOrderedIcon />
+            <p>Delivery & Return</p>
+          </div>
+          <div className="flex items-center gap-2 text-sm text-black hover:text-red-600 hoverEffect">
+            <Share />
+            <p>Share</p>
+          </div>
+        </div>
+        <div className="flex flex-wrap items-centeer gap-5">
+          <div className="border boreder-darkBlue/20 text-center p-3 hover:border-darkBlue rounded-md hoverEffect">
+            <p className="text-base font-semibold text-darkColor">
+              Free Shipping
+            </p>
+            <p className="text-sm text-gray-500">
+              Free shipping order over $120
+            </p>
+          </div>
+          <div className="border boreder-darkBlue/20 text-center p-3 hover:border-darkBlue rounded-md hoverEffect">
+            <p className="text-base font-semibold text-darkColor">
+              Free Shipping
+            </p>
+            <p className="text-sm text-gray-500">
+              Free shipping order over $120
+            </p>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 export default SingleProductPage;
