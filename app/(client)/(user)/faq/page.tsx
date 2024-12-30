7f70eb758f9e086f5c938805b8e0c240f9cbcf4c
Oyelaja Hussein <premiumitng@gmail.com>
2024-12-30 23:32:56 +0100

    Completed and added Accordion to the FAQ page

--- app/(client)/(user)/faq/page.tsx
+++ app/(client)/(user)/faq/page.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Container from '@/components/Container'
+import React from "react";
+import Container from "@/components/Container";
+import Title from "@/components/Title";
+import { faqsData } from "@/constants";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const FaqPage = () => {
   return (
-    <Container>
-        <div>faqPage</div>
+    <Container className="max-w-4xl sm:px-6 lg:px-8 py-12">
+      <Title className="text-3xl">Frequently Asked Questions</Title>
+      <Accordion type="single" collapsible className="w-full" defaultValue="item-0">
+        {faqsData?.map((faq, index) => (
+          <AccordionItem key={index} value={`item-${index}`} className="group">
+            <AccordionTrigger className="text-left text-lg font-semibold text-darkColor/80 group-hover:text-darkColor hover:no-underline hoverEffect">{faq?.question}</AccordionTrigger>
+            <AccordionContent className="text-gray-600">{faq?.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </Container>
-  )
-}
+  );
+};
 
-export default FaqPage
+export default FaqPage;
