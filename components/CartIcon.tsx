f7d3dc21e46cf086a3e88c9971643ad27e111bce
Oyelaja Hussein <premiumitng@gmail.com>
2024-12-31 18:28:53 +0100

    Updated the cart icon counter to update when add to cart but is clicked

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -1,12 +1,17 @@
+'use client'
 import { ShoppingBag } from 'lucide-react'
 import React from 'react'
 import Link from 'next/link'
+import useCartStore from '@/store'
 
 const CartIcon = () => {
+  const {items} = useCartStore()
   return (
     <Link href={"/cart"} className='group relative'>
 <ShoppingBag className="w-5 h-5 group-hover:text-darkColor hoverEffect" />
-<span className='absolute -top-1 -right-1 bg-darkColor text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center'>0</span>
+<span className='absolute -top-1 -right-1 bg-red-600 text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center'>
+      {items.length?items.length:0}
+</span>
     </Link>
   )
 }
